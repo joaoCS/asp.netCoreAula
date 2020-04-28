aedbf22eab171ae6e8aa0a8d9b65c5c15cb55e0e
JoaoCS <joaoantoniogba025@gmail.com>
2020-04-28 18:37:01 -0300

    Adicionados todos os arquivos

---
+++ aulaASPCore/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aulaASPCore.Models
+{
+    public class Cliente
+    {
+        public Cliente() { }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Telefone { get; set; }
+    }
+}
